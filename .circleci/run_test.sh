4e4ae782047846500ed8835e3e66c7420040ce80
Jose Escudero <josescuderoh@icloud.com>
2022-11-02 12:57:42 -0400

    Added profile setup

---
+++ .circleci/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Show location of local install of dbt
+echo $(which dbt)
+
+# Show version and installed adapters
+dbt --version
+
+# Set the profile
+cd .cicd
+cp sample.profiles.yml profiles.yml
+export DBT_PROFILES_DIR=.
+
+# Show the location of the profiles directory and test the connection
+dbt debug --target $1
