820edf22d6f0c56a1edc5bd5220af01a2cfba1fa
Jose Escudero <josescuderoh@icloud.com>
2022-11-02 18:19:04 -0400

    Removed debug step

--- .circleci/run_test.sh
+++ .circleci/run_test.sh
@@ -6,8 +6,6 @@ echo $(which dbt)
 # Show version and installed adapters
 dbt --version
 
-echo $SNOWFLAKE_ACCOUNT
-
 # Set the profile
 cd .circleci
 cp sample.profiles.yml profiles.yml
@@ -15,4 +13,4 @@ export DBT_PROFILES_DIR=.
 
 # Show the location of the profiles directory and test the connection
 # dbt debug --profile cicd_pipeline --target $1
-dbt docs generate # --profile cicd_pipeline --target $1
+dbt docs generate --profile cicd_pipeline --target $1
